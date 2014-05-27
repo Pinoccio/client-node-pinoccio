d9834b5276caf94200c7eb30d2e6ef11897340e5
Ryan Day <soldair@gmail.com>
2014-05-26 17:16:15 -0700

    fixing token

--- test/sync.js
+++ test/sync.js
@@ -3,15 +3,14 @@ var pinoccio = require('../');
 
 test("can sync",function(t){
   // add a test readonly token.
-  api = pinoccio("e113fa4270ae050a0c706d29eca5ad25");
+  api = pinoccio("1cc899dd27a391f0e58d03d4f290fef2");
 
   api.sync({stale:1}).once('data',function(data){
     t.ok(data,'got data')
     this.end();
   }).on('end',function(){
     t.end();
-  });
-
+  })
 
 });
 
